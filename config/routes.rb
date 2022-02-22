ec4b0d335021d9d8ea4621cd39a2eb3a6bc3d791
EC2 Default User <ec2-user@ip-172-31-4-87.ec2.internal>
2022-02-22 10:10:54 +0000

    lesson14_finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  
-  resources :postimages, only:[:new, :index, :show, :create, :destroy]
+
   devise_for :users
   root to: "homes#top"
   get 'homes/about' => 'homes#about'
-  get 'meshis/new'
-  get 'meshis/index'
-  get 'meshis/show'
-  get 'meshis/edit'
+  resources :postimages, only:[:new, :index, :show, :create, :destroy]
+  resources :users, only:[:show, :edit]
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
