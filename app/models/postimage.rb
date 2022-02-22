1e270558337b383c59ce977e64070c14f62d07d2
EC2 Default User <ec2-user@ip-172-31-4-87.ec2.internal>
2022-02-22 03:33:10 +0000

    lesson10_finish

--- app/models/postimage.rb
+++ app/models/postimage.rb
@@ -1,5 +1,12 @@
 class Postimage < ApplicationRecord
   has_one_attached :image
-  
   belongs_to :user
+  
+  def get_image
+    if image.attached?
+      image
+    else
+      'no_image.jpg'
+    end
+  end
 end
