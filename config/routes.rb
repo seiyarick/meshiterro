da4f329657391ccb31d182b70448ceb614777a32
EC2 Default User <ec2-user@ip-172-31-4-87.ec2.internal>
2022-02-22 01:32:16 +0000

    lesson9_finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  
+  resources :postimages, only:[:new, :index, :show]
   devise_for :users
   root to: "homes#top"
   get 'homes/about' => 'homes#about'
--- db/migrate/20220221002016_create_postimages.rb
+++ db/migrate/20220221002016_create_postimages.rb
@@ -1,7 +1,7 @@
 class CreatePostimages < ActiveRecord::Migration[6.1]
   def change
     create_table :postimages do |t|
-     
+      t.integer :id
       t.string :shop_name
       t.text :caption
       t.integer :user_id
